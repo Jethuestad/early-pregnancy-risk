d03b1b933639dea81879c77ad21badf56028d573
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2021-02-13 13:30:35 +0100

    Added error names to exceptions

--- server/epr/epr/exceptions/api_exceptions.py
+++ server/epr/epr/exceptions/api_exceptions.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
 
 def bad_json_exception():
-    return _exception_constructor("Ill-formated JSON! Please reformat and try again", 400)
+    return _exception_constructor("bad_json_exception", "Ill-formated JSON! Please reformat and try again", 400)
 
-def _exception_constructor(error_msg: str, error_code: int):
-    error_dict = {"Error": {"Message": error_msg, "Code": error_code}}
+def _exception_constructor(error_name: str, error_msg: str, error_code: int):
+    error_dict = {"Error": {"Name": error_name, "Message": error_msg, "Code": error_code}}
     return JsonResponse(error_dict, safe=False, status = error_code)
