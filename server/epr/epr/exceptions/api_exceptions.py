6887660e9c314baffbb6aa1bbab0159c59b80be0
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2021-02-13 15:11:38 +0100

    Added bad_method_exception

--- server/epr/epr/exceptions/api_exceptions.py
+++ server/epr/epr/exceptions/api_exceptions.py
@@ -1,7 +1,11 @@
 from django.http import JsonResponse
 
 def bad_json_exception():
-    return _exception_constructor("bad_json_exception", "Ill-formated JSON! Please reformat and try again", 400)
+    return _exception_constructor("Bad Json", "Ill-formated JSON! Please reformat and try again", 400)
+
+def bad_method_exception(method_got: str, method_expected: str):
+    return _exception_constructor("Method Not Allowed", 
+        "Expected {} method but got {} method insteads".format(method_expected, method_got), 405)
 
 def _exception_constructor(error_name: str, error_msg: str, error_code: int):
     error_dict = {"Error": {"Name": error_name, "Message": error_msg, "Code": error_code}}
