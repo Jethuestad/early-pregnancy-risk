80ab91a6a485b974ffc5e18a3bf606b998becce7
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2021-02-13 15:11:55 +0100

    Method check added to score_calculator

--- server/epr/epr/services/score_calculator.py
+++ server/epr/epr/services/score_calculator.py
@@ -1,10 +1,13 @@
 from ..utilities.request_utils import body_to_dict
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from ..exceptions.api_exceptions import bad_json_exception
+from ..exceptions.api_exceptions import *
 
 @csrf_exempt
 def get_score(request):
+    if request.method != "POST":
+        return bad_method_exception(request.method, "POST")
+
     try:
         print(body_to_dict(request))
     except:
