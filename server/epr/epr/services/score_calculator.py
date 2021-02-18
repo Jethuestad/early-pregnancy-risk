6a047767d9bb2cbfbd388773c3354ffb8b4e29b1
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2021-02-18 15:41:37 +0100

    Name changes

--- server/epr/epr/services/score_calculator.py
+++ server/epr/epr/services/score_calculator.py
@@ -1,18 +1,16 @@
-from ..utilities.request_utils import body_to_dict
+from ..utilities.request_utils import json_request_to_dict
 from django.views.decorators.csrf import csrf_exempt
 from ..utilities.decorators import exception_handler_request
 from django.http import JsonResponse
 from ..exceptions.api_exceptions import BadMethodException
+from ..utilities.calculator_json_format import valid_json_format_calc
 
 @csrf_exempt
 @exception_handler_request
 def get_score(request):
     if request.method != "POST":
         raise BadMethodException(request.method, "POST")
-
-    print(body_to_dict(request))
-    json_dict = body_to_dict(request)
-
+    
+    answer_dict = json_request_to_dict(request, valid_json_format_calc)
     # TODO: Add proper json response
-    return JsonResponse(body_to_dict(request), safe=False)
-
+    return JsonResponse(answer_dict, safe=False)
