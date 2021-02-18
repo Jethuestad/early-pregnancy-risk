e69682d139fc50ea9ac4fd503161a93f7f16429f
Thomas Storli <thomas.storli@live.no>
2021-02-18 12:53:14 +0100

    Added a simple button to test if the requests are working.

--- client/src/EarlyPregnancyRisk/App.js
+++ client/src/EarlyPregnancyRisk/App.js
@@ -1,11 +1,40 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import PageName from "./components/PageName";
+import { getTestJson } from "./networking/Requests";
 
 export default function App() {
+  const [isLoading, setLoading] = useState(false);
+  const [testRequest, setTestRequest] = useState(null);
+
+  useEffect(() => {
+    (async function () {
+      if (isLoading) {
+        try {
+          const json = await getTestJson();
+          setTestRequest(json);
+        } finally {
+          setLoading(false);
+        }
+      }
+    })();
+  }, [isLoading]);
+
   return (
     <View style={styles.container}>
       <PageName />
+
+      {/* Test code to show a request */}
+      <TouchableOpacity onPress={() => setLoading(true)} style={styles.button}>
+        <Text style={styles.buttonText}>Make Request</Text>
+      </TouchableOpacity>
+      <View style={styles.centre}>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : (
+          <Text>{testRequest != null ? testRequest.title : "no content"}</Text>
+        )}
+      </View>
     </View>
   );
 }
@@ -14,5 +43,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  button: {
+    width: "15%",
+    height: "30px",
+    alignSelf: "center",
+    margin: 10,
+    backgroundColor: "#000",
+  },
+  buttonText: { color: "white", textAlign: "center", paddingTop: "4px" },
+  centre: {
+    alignSelf: "center",
   },
 });
