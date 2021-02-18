cb4f2dd8f73311af39de1f6e6cbcb2a60524e58e
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2021-02-18 15:41:26 +0100

    Updated json processing method to also check for correct formatting

--- server/epr/epr/utilities/request_utils.py
+++ server/epr/epr/utilities/request_utils.py
@@ -1,9 +1,25 @@
 import json
 from ..exceptions.api_exceptions import BadJsonException
 
-def body_to_dict(request) -> dict:
+def json_request_to_dict(request, valid_format: dict) -> dict:
     '''Turns a JSON field in a POST request in to a dictornary'''
     try:
-        return json.loads(request.body)
+        request_dict = json.loads(request.body)
     except:
         raise BadJsonException()
+
+    if len(request_dict) != len(valid_format):
+        print("Invalid length of dict")
+        raise BadJsonException()
+    
+    for key, value in request_dict.items():
+        if key not in valid_format:
+            print("The key \"{}\" should not exist".format(key))
+            raise BadJsonException()
+
+        if type(value) != valid_format[key]:
+            print("Invalid type of element located in {}. Should be {} but is {}"
+                .format(key, valid_format[key], type(value)))
+            raise BadJsonException()
+    
+    return request_dict
