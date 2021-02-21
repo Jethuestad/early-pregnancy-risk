7bd0a6d64801ef4abc42221bc4ebd559599d7a8b
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2021-02-21 13:06:14 +0100

    Added a standardized JSON reply both on success and failure

--- server/epr/epr/utilities/request_utils.py
+++ server/epr/epr/utilities/request_utils.py
@@ -1,25 +1,10 @@
-import json
-from ..exceptions.api_exceptions import BadJsonException
+from django.http import JsonResponse
 
-def json_request_to_dict(request, valid_format: dict) -> dict:
-    '''Turns a JSON field in a POST request in to a dictornary'''
-    try:
-        request_dict = json.loads(request.body)
-    except:
-        raise BadJsonException()
-
-    if len(request_dict) != len(valid_format):
-        print("Invalid length of dict")
-        raise BadJsonException()
-    
-    for key, value in request_dict.items():
-        if key not in valid_format:
-            print("The key \"{}\" should not exist".format(key))
-            raise BadJsonException()
-
-        if type(value) != valid_format[key]:
-            print("Invalid type of element located in {}. Should be {} but is {}"
-                .format(key, valid_format[key], type(value)))
-            raise BadJsonException()
-    
-    return request_dict
+def standard_json_response(success: bool, payload: dict, status_code=200):
+    response = {"success": success}
+    if not success:
+        response["error"] = payload
+    else:
+        response["payload"] = payload
+    print(response)
+    return JsonResponse(response, safe=False, status=status_code)
