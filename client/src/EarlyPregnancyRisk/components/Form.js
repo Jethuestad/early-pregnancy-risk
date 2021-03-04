1b8711f1d440803eb3a70f170b7ad81deadc7804
Thomas Storli <thomas.storli@live.no>
2021-03-04 17:36:20 +0100

    Removed progress state, as it does the same as nr

--- client/src/EarlyPregnancyRisk/components/Form.js
+++ client/src/EarlyPregnancyRisk/components/Form.js
@@ -26,15 +26,7 @@ export default function Form() {
   const [data, setData] = useState({});
   const [risk, setRisk] = useState(null);
 
-
   let animation = useRef(new Animated.Value(0));
-  const [progress, setProgress] = useState(1);
-
-  function prog(){
-    if(progress < factors.length) {
-      setProgress(progress + 1);
-    }
-  }
 
   const TestResponse = {
     success: true,
@@ -91,7 +83,6 @@ export default function Form() {
       setData(tData);
       addSubFactors();
     }
-    prog()
     setSkipped(false);
     setFactorInteger("");
     setIsSubmitting(false);
@@ -109,141 +100,140 @@ export default function Form() {
 
   useEffect(() => {
     Animated.timing(animation.current, {
-      toValue: progress,
+      toValue: nr,
       duration: factors.length,
       useNativeDriver: false,
     }).start();
-  }, [progress]);
+  }, [nr]);
 
   const width = animation.current.interpolate({
     inputRange: [0, factors.length],
-    outputRange: ["0%","100%"],
+    outputRange: ["0%", "100%"],
     extrapolate: "clamp",
   });
 
   return (
-      <View style={styles.main_container}>
-        {nr < factors.length ?
+    <View style={styles.main_container}>
+      {nr < factors.length ? (
+        <View style={progBarStyles.container}>
+          <View style={progBarStyles.progressBar}>
+            <Animated.View
+              style={[
+                StyleSheet.absoluteFill,
+                { backgroundColor: "#E15A46", width },
+              ]}
+            />
+          </View>
+          <Text styles={{ fontSize: 15 }}>{`${nr}/${factors.length}`}</Text>
+        </View>
+      ) : null}
 
-            <View style={progBarStyles.container}>
-              <View style={progBarStyles.progressBar}>
-                <Animated.View
-                    style={[
-                      StyleSheet.absoluteFill,
-                      { backgroundColor: "#E15A46", width },
-                    ]}
-                />
-              </View>
-              <Text styles={{fontSize:15}}>{`${progress}/24`}</Text>
-            </View>
-            : null}
-
-        <View style={styles.container}>
-          {risk != null ? (
-              <Results risk={risk} />
-          ) : (
+      <View style={styles.container}>
+        {risk != null ? (
+          <Results risk={risk} />
+        ) : (
+          <View>
+            {nr < factors.length ? (
               <View>
-                {nr < factors.length ? (
+                <Text style={[styles.question, colors.primary]}>
+                  {factors[nr].question}
+                </Text>
+                <View style={styles.spacing} />
+                {/* NUMERICAL*/}
+                {factors[nr].answertype === "int" ? (
+                  <View>
+                    <TextInput
+                      style={styles.textinput}
+                      onChangeText={(value) =>
+                        setFactorInteger(value.replace(/[^0-9]/g, ""))
+                      }
+                      numeric
+                      keyboardType="numeric"
+                      value={factorInteger}
+                      maxLength={factors[nr].maxdigits}
+                    ></TextInput>
+                    <View style={styles.spacingBtn} />
+                    <TouchableHighlight
+                      style={styles.inputBtn}
+                      activeOpacity={0.6}
+                      underlayColor="#DDDDDD"
+                      onPress={() =>
+                        factorInteger == ""
+                          ? setIsSubmitting(false)
+                          : setIsSubmitting(true)
+                      }
+                    >
+                      <Text style={styles.textTitleBtn}>Continue</Text>
+                    </TouchableHighlight>
+                  </View>
+                ) : null}
+                {/* YES or NO*/}
+                {factors[nr].answertype === "boolean" ? (
+                  <View style={styles.spacingBtn}>
                     <View>
-                      <Text style={[styles.question, colors.primary]}>
-                        {factors[nr].question}
-                      </Text>
-                      <View style={styles.spacing} />
-                      {/* NUMERICAL*/}
-                      {factors[nr].answertype === "int" ? (
-                          <View>
-                            <TextInput
-                                style={styles.textinput}
-                                onChangeText={(value) =>
-                                    setFactorInteger(value.replace(/[^0-9]/g, ""))
-                                }
-                                numeric
-                                keyboardType="numeric"
-                                value={factorInteger}
-                                maxLength={factors[nr].maxdigits}
-                            ></TextInput>
-                            <View style={styles.spacingBtn} />
-                            <TouchableHighlight
-                                style={styles.inputBtn}
-                                activeOpacity={0.6}
-                                underlayColor="#DDDDDD"
-                                onPress={() =>
-                                    factorInteger == ""
-                                        ? setIsSubmitting(false)
-                                        : setIsSubmitting(true)
-                                }
-                            >
-                              <Text style={styles.textTitleBtn}>Continue</Text>
-                            </TouchableHighlight>
-                          </View>
-                      ) : null}
-                      {/* YES or NO*/}
-                      {factors[nr].answertype === "boolean" ? (
-                          <View style={styles.spacingBtn}>
-                            <View>
-                              <TouchableHighlight
-                                  style={styles.inputBtn}
-                                  activeOpacity={0.6}
-                                  underlayColor="#DDDDDD"
-                                  onPress={() => {
-                                    setFactorBoolean(false);
-                                    setIsSubmitting(true);
-                                  }}
-                              >
-                                <Text style={styles.textTitleBtn}>No</Text>
-                              </TouchableHighlight>
-                            </View>
-                            <View style={styles.spacingBtn} />
-                            <View>
-                              <TouchableHighlight
-                                  style={styles.inputBtn}
-                                  activeOpacity={0.6}
-                                  underlayColor="#DDDDDD"
-                                  onPress={() => {
-                                    setFactorBoolean(true);
-                                    setIsSubmitting(true);
-                                  }}
-                              >
-                                <Text style={styles.textTitleBtn}>Yes</Text>
-                              </TouchableHighlight>
-                            </View>
-                          </View>
-                      ) : null}
-                      {factors[nr].skippable ? (
-                          <View>
-                            <View style={styles.spacingBtn} />
-                            <TouchableHighlight
-                                style={styles.inputBtn}
-                                activeOpacity={0.6}
-                                underlayColor="#DDDDDD"
-                                onPress={() => {
-                                  setSkipped(true);
-                                  setIsSubmitting(true);
-                                }}
-                            >
-                              <Text style={styles.skipBtn}>Skip</Text>
-                            </TouchableHighlight>
-                          </View>
-                      ) : null}
+                      <TouchableHighlight
+                        style={styles.inputBtn}
+                        activeOpacity={0.6}
+                        underlayColor="#DDDDDD"
+                        onPress={() => {
+                          setFactorBoolean(false);
+                          setIsSubmitting(true);
+                        }}
+                      >
+                        <Text style={styles.textTitleBtn}>No</Text>
+                      </TouchableHighlight>
                     </View>
+                    <View style={styles.spacingBtn} />
+                    <View>
+                      <TouchableHighlight
+                        style={styles.inputBtn}
+                        activeOpacity={0.6}
+                        underlayColor="#DDDDDD"
+                        onPress={() => {
+                          setFactorBoolean(true);
+                          setIsSubmitting(true);
+                        }}
+                      >
+                        <Text style={styles.textTitleBtn}>Yes</Text>
+                      </TouchableHighlight>
+                    </View>
+                  </View>
+                ) : null}
+                {factors[nr].skippable ? (
+                  <View>
+                    <View style={styles.spacingBtn} />
+                    <TouchableHighlight
+                      style={styles.inputBtn}
+                      activeOpacity={0.6}
+                      underlayColor="#DDDDDD"
+                      onPress={() => {
+                        setSkipped(true);
+                        setIsSubmitting(true);
+                      }}
+                    >
+                      <Text style={styles.skipBtn}>Skip</Text>
+                    </TouchableHighlight>
+                  </View>
+                ) : null}
+              </View>
+            ) : (
+              <View>
+                {Platform.OS !== "web" ? (
+                  <View>
+                    <Results risk={TestResponse} />
+                    <Text>
+                      This is a test response, it does not come from the server.
+                    </Text>
+                  </View>
                 ) : (
-                    <View>
-                      {Platform.OS !== "web" ? (
-                          <View>
-                            <Results risk={TestResponse} />
-                            <Text>
-                              This is a test response, it does not come from the server.
-                            </Text>
-                          </View>
-                      ) : (
-                          <Text>Loading...</Text>
-                      )}
-                    </View>
+                  <Text>Loading...</Text>
                 )}
               </View>
-          )}
-        </View>
+            )}
+          </View>
+        )}
       </View>
+    </View>
   );
 }
 
@@ -373,7 +363,7 @@ const progBarStyles = StyleSheet.create({
     alignItems: "center",
     borderBottomColor: "black",
     borderBottomWidth: 2,
-    maxHeight: 40
+    maxHeight: 40,
   },
   progressBar: {
     flexDirection: "row",
