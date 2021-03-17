aeb39c570aa11d00af22cd4ff9b4b43ce986581d
Sindre <sindrerk@me.com>
2021-03-17 13:00:30 +0100

    fixed ann error

--- client/src/EarlyPregnancyRisk/components/FormOverlay.js
+++ client/src/EarlyPregnancyRisk/components/FormOverlay.js
@@ -8,6 +8,7 @@ export default function FormOverlay({ factor, visible, setVisible }) {
   Modal.setAppElement("body");
   return (
     <View>
+
       {visible ? (
         Platform.OS === "web" ? (
           <Overlay
