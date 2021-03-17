622d5cd6ef4fa66fcd9387f6b7a67f2e88344c6a
Sindre <sindrerk@me.com>
2021-03-17 11:39:30 +0100

    using modal react native

--- client/src/EarlyPregnancyRisk/components/FormOverlay.js
+++ client/src/EarlyPregnancyRisk/components/FormOverlay.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { View, Platform, StyleSheet } from "react-native";
-import { Overlay } from "react-native-elements";
 import ReferenceList from "./ReferenceList";
 import Modal from "modal-react-native-web";
+import {Button} from "react-native-web";
+import colors from "../style/colors";
 
 export default function FormOverlay({ factor, visible, setVisible }) {
   Modal.setAppElement("body");
@@ -11,28 +12,93 @@ export default function FormOverlay({ factor, visible, setVisible }) {
       {visible ? (
         Platform.OS === "web" ? (
           <Modal
-            animationType="slide"
-            transparent={true}
-            visible={visible}
-          >
-            <ReferenceList
-              factor_name={factor.factor}
-              close={() => setVisible(false)}
-            />
-          </Modal>
-        ) : (
-          <Modal
               animationType="slide"
               transparent={true}
               visible={visible}
+              presentationStyle={"pageSheet"}
           >
-            <ReferenceList
-              factor_name={factor.factor}
-              close={() => setVisible(false)}
-            />
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <ReferenceList
+                  factor_name={factor.factor}
+                  close={() => setVisible(false)}
+                />
+                <Button onPress={() => setVisible(false)} title={"Close ref"} color={colors.primary} />
+              </View>
+            </View>
           </Modal>
+        ) : (
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={visible}
+            >
+              <View style={styles.centeredViewPhone}>
+                <View style={styles.modalView}>
+                  <ReferenceList
+                      factor_name={factor.factor}
+                      close={() => setVisible(false)}
+                  />
+                  <Button onPress={() => setVisible(false)} title={"Close ref"} color={"#BF3B29"} />
+                </View>
+              </View>
+            </Modal>
         )
       ) : null}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+    maxHeight: "50%",
+  },
+  centeredViewPhone: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    marginTop: 22,
+    width: "80%",
+    height: "90%"
+  },
+  modalView: {
+    flex: 1,
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalViewPhone:{
+    flex: 1,
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    justifyContent: "center",
+    alignContent: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width: "80%",
+    height: "90%"
+  }
+});
--- client/src/EarlyPregnancyRisk/components/ReferenceList.js
+++ client/src/EarlyPregnancyRisk/components/ReferenceList.js
@@ -23,54 +23,27 @@ export default function ReferenceList({ factor_name, close }) {
 
   if (references == null || isLoading) {
     return (
-      <View
-        style={{
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100%",
-        }}
-      >
-        <Loading message="Loading references..." />
-      </View>
+        <View
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100%",
+            }}
+        >
+          <Loading message="Loading references..." />
+        </View>
     );
   }
 
   return (
-      <View style={styles.centeredView}>
-        <View style={styles.modalView}>
+      <View>
         {references.map((val) => (
-          <ListItem bottomView key={val.id}>
-            <ListItem.Content>
-              <Text>{val.ref}</Text>
-            </ListItem.Content>
-          </ListItem>
-        ))}
-      </View>
+              <ListItem bottomView key={val.id}>
+                <ListItem.Content>
+                  <Text>{val.ref}</Text>
+                </ListItem.Content>
+              </ListItem>
+          ))}
       </View>
   );
 }
-
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
-  },
-});
