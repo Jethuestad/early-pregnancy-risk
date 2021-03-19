337f58169cc91c60b3e7cc15c0ca91085e99be0e
Thomas Storli <thomas.storli@live.no>
2021-03-19 23:07:38 +0100

    Fixed that severity started at 1 instead of 0.

--- client/src/EarlyPregnancyRisk/modules/Severity.js
+++ client/src/EarlyPregnancyRisk/modules/Severity.js
@@ -1,15 +1,14 @@
 export function getSeverity(s) {
-
   switch (s) {
-    case 1:
+    case 0:
       return "Low";
-    case 2:
+    case 1:
       return "Increased";
-    case 3:
+    case 2:
       return "Moderate";
-    case 4:
+    case 3:
       return "High";
-    case 5:
+    case 4:
       return "Very High";
     default:
       return "Error";
