e0b92605d4c9f948dd270cacbd8c3a57cb0252e7
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2021-04-10 15:50:15 +0200

    Log more information (IP's, and request body when the request caused an unhandled error)

--- server/epr/epr/utilities/decorators.py
+++ server/epr/epr/utilities/decorators.py
@@ -1,7 +1,7 @@
 from ..exceptions.api_exceptions import InternalServerError, exception_response_constructor
 from rest_framework.exceptions import APIException
 import logging
-logger = logging.getLogger("django")
+logger = logging.getLogger("django.request")
 
 def exception_handler_request(function, *args, **kwargs):
     '''This decorator handles otherwise unhandled errors and returns a generic error message'''
@@ -10,11 +10,10 @@ def exception_handler_request(function, *args, **kwargs):
             return function(request, *args, **kwargs)
 
         except APIException as handled_exception:
-            logger.warning(handled_exception, exc_info=1)
+            logger.warning("\nip:{}\n\nendpoint:{}\nstack trace:{}".format(request.META.get('REMOTE_ADDR'), request.headers ,handled_exception), exc_info=1)
             return exception_response_constructor(handled_exception)
 
         except Exception as unhandled_exception:
-            # TODO: Implement proper logging
-            logger.error(unhandled_exception, exc_info=1)
+            logger.error("\nip:{}\nbody:{}\nendpoint:{}\nstack trace:{}".format(request.META.get('REMOTE_ADDR'), request.body,request.headers ,uhandled_exception), exc_info=1)
             return exception_response_constructor(InternalServerError())
     return _function
