c35674befb975c74947ade8ad45bde2ec646b847
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2021-04-22 15:52:18 +0200

    Added correct encoding for data

--- server/epr/epr/settings.py
+++ server/epr/epr/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from os import path, mkdir
+import _locale
+
+# Correctly encode UTF-8 characters
+_locale._getdefaultlocale = (lambda *args: ['en_US', 'utf8'])
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
