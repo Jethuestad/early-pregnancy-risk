dda883dca3ea485680172df7adb1fd472565c4c5
Alexander Kvamme <alexanderdebattistakvamme@gmail.com>
2021-04-22 13:56:17 +0200

    Added Disease and Reference to admin menu

--- server/epr/epr/admin.py
+++ server/epr/epr/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Language, Translation, ContentType, Content, AnswerType, Factor
+from .models import Language, Translation, ContentType, Content, AnswerType, Factor, Disease, References
 
 
 class TranslationAdmin(admin.ModelAdmin):
@@ -16,3 +16,5 @@ admin.site.register(ContentType)
 admin.site.register(Content, ContentAdmin)
 admin.site.register(AnswerType)
 admin.site.register(Factor)
+admin.site.register(Disease)
+admin.site.register(References)
