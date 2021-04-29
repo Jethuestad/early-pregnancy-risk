4c3afab64c9658b19c333a377e9ce85b173af3ec
Jethuestad <57411743+Jethuestad@users.noreply.github.com>
2021-04-29 13:38:13 +0200

    Update preeclampsia.py small factor name change

--- server/epr/epr/services/complications/preeclampsia.py
+++ server/epr/epr/services/complications/preeclampsia.py
@@ -68,125 +68,125 @@ def calculate(json_dict: dict) -> dict:
         risk_score += 8
 
     # Parous with previous PE (PPE)
-    if json_dict.get("ppe"):
+    if json_dict.get("parous_ppe"):
         risk_score += 39
 
-    # If PPE=1:
-    # Gestational age
-    if (g_age:=json_dict.get("g_age")) == None:
-        pass
-    elif g_age < 26:
-        risk_score += 0
-    elif g_age == 26:
-        risk_score -= 1
-    elif g_age == 27:
-        risk_score -= 1
-    elif g_age == 28:
-        risk_score -= 2
-    elif g_age == 29:
-        risk_score -= 3
-    elif g_age == 30:
-        risk_score -= 5
-    elif g_age == 31:
-        risk_score -= 6
-    elif g_age == 32:
-        risk_score -= 8
-    elif g_age == 33:
-        risk_score -= 11
-    elif g_age == 34:
-        risk_score -= 13
-    elif g_age == 35:
-        risk_score -= 16
-    elif g_age == 36:
-        risk_score -= 19
-    elif g_age == 37:
-        risk_score -= 22
-    elif g_age == 38:
-        risk_score -= 26
-    elif g_age == 39:
-        risk_score -= 30
-    elif g_age == 40:
-        risk_score -= 34
-    elif g_age > 40:
-        risk_score -= 39
+        # If PPE=1:
+        # Gestational age
+        if (g_age:=json_dict.get("g_age")) == None:
+            pass
+        elif g_age < 26:
+            risk_score += 0
+        elif g_age == 26:
+            risk_score -= 1
+        elif g_age == 27:
+            risk_score -= 1
+        elif g_age == 28:
+            risk_score -= 2
+        elif g_age == 29:
+            risk_score -= 3
+        elif g_age == 30:
+            risk_score -= 5
+        elif g_age == 31:
+            risk_score -= 6
+        elif g_age == 32:
+            risk_score -= 8
+        elif g_age == 33:
+            risk_score -= 11
+        elif g_age == 34:
+            risk_score -= 13
+        elif g_age == 35:
+            risk_score -= 16
+        elif g_age == 36:
+            risk_score -= 19
+        elif g_age == 37:
+            risk_score -= 22
+        elif g_age == 38:
+            risk_score -= 26
+        elif g_age == 39:
+            risk_score -= 30
+        elif g_age == 40:
+            risk_score -= 34
+        elif g_age > 40:
+            risk_score -= 39
 
     # Parous with no previous PE
-    if json_dict.get("pnpe"):
+    if json_dict.get("parous_no_ppe"):
         risk_score += 21
     
-    # If PPE=0
-    # Inter-pregnancy interval (years)
-    if (ip_interval:=json_dict.get("inter-pregnancy_interval")) == None:
-        pass
-    elif ip_interval == 1:
-        risk_score -= 25
-    elif ip_interval == 2:
-        risk_score -= 21
-    elif ip_interval == 3:
-        risk_score -= 19
-    elif ip_interval == 4:
-        risk_score -= 17
-    elif ip_interval == 5:
-        risk_score -= 16
-    elif ip_interval == 6:
-        risk_score -= 15
-    elif ip_interval == 7:
-        risk_score -= 14
-    elif 8 <= ip_interval <= 9:
-        risk_score -= 13
-    elif ip_interval == 10:
-        risk_score -= 12
-    elif 11 <= ip_interval <= 12:
-        risk_score -= 11
-    elif 13 <= ip_interval <= 17:
-        risk_score -= 10
-    elif ip_interval >= 18:
-        risk_score -= 9
+        # If PPE=0
+        # Inter-pregnancy interval (years)
+        if (ip_interval:=json_dict.get("inter-pregnancy_interval")) == None:
+            pass
+        elif ip_interval == 1:
+            risk_score -= 25
+        elif ip_interval == 2:
+            risk_score -= 21
+        elif ip_interval == 3:
+            risk_score -= 19
+        elif ip_interval == 4:
+            risk_score -= 17
+        elif ip_interval == 5:
+            risk_score -= 16
+        elif ip_interval == 6:
+            risk_score -= 15
+        elif ip_interval == 7:
+            risk_score -= 14
+        elif 8 <= ip_interval <= 9:
+            risk_score -= 13
+        elif ip_interval == 10:
+            risk_score -= 12
+        elif 11 <= ip_interval <= 12:
+            risk_score -= 11
+        elif 13 <= ip_interval <= 17:
+            risk_score -= 10
+        elif ip_interval >= 18:
+            risk_score -= 9
 
-    # If PPE=0  
-    # Gestational age
-    if (g_age:=json_dict.get("g_age")) == None:
-        pass
-    elif g_age < 27:
-        risk_score += 0
-    elif g_age == 27:
-        risk_score -= 1
-    elif g_age == 28:
-        risk_score -= 1
-    elif g_age == 29:
-        risk_score -= 2
-    elif g_age == 30:
-        risk_score -= 3
-    elif g_age == 31:
-        risk_score -= 4
-    elif g_age == 32:
-        risk_score -= 5
-    elif g_age == 33:
-        risk_score -= 6
-    elif g_age == 34:
-        risk_score -= 7
-    elif g_age == 35:
-        risk_score -= 9
-    elif g_age == 36:
-        risk_score -= 11
-    elif g_age == 37:
-        risk_score -= 13
-    elif g_age == 38:
-        risk_score -= 15
-    elif g_age == 39:
-        risk_score -= 17
-    elif g_age == 40:
-        risk_score -= 19
-    elif g_age == 41:
-        risk_score -= 22
-    elif g_age == 42:
-        risk_score -= 24
-    elif g_age >= 43:
-        risk_score -= 27
+        # If PPE=0  
+        # Gestational age
+        if (g_age:=json_dict.get("g_age")) == None:
+            pass
+        elif g_age < 27:
+            risk_score += 0
+        elif g_age == 27:
+            risk_score -= 1
+        elif g_age == 28:
+            risk_score -= 1
+        elif g_age == 29:
+            risk_score -= 2
+        elif g_age == 30:
+            risk_score -= 3
+        elif g_age == 31:
+            risk_score -= 4
+        elif g_age == 32:
+            risk_score -= 5
+        elif g_age == 33:
+            risk_score -= 6
+        elif g_age == 34:
+            risk_score -= 7
+        elif g_age == 35:
+            risk_score -= 9
+        elif g_age == 36:
+            risk_score -= 11
+        elif g_age == 37:
+            risk_score -= 13
+        elif g_age == 38:
+            risk_score -= 15
+        elif g_age == 39:
+            risk_score -= 17
+        elif g_age == 40:
+            risk_score -= 19
+        elif g_age == 41:
+            risk_score -= 22
+        elif g_age == 42:
+            risk_score -= 24
+        elif g_age >= 43:
+            risk_score -= 27
 
     
     # Weight (kg)
-    if (weight:=json_dict.get("height")) == None:
+    if (weight:=json_dict.get("weight")) == None:
         pass
     elif weight < 45:
         risk_score += 0
