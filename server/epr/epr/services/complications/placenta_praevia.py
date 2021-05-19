c0c6a7fb65f37ebe2f52cd58a3874f5d9770b86a
Jethuestad <57411743+Jethuestad@users.noreply.github.com>
2021-05-19 14:30:51 +0200

    placental complications added

---
+++ server/epr/epr/services/complications/placenta_praevia.py
@@ -0,0 +1,68 @@
+from typing import get_args
+from ...exceptions.api_exceptions import InternalServerError
+
+
+# Returning the risk_score and the severity of the risk_score
+def placenta_praevia_risk(risk_score: int) -> dict:
+    # TODO: This needs to be specified.
+    return
+
+# Calculating risk score unique for post-partum depression
+
+
+def calculate(json_dict: dict) -> dict:
+    risk_score = 0
+
+
+    # Maternal age
+    if (maternal_age := json_dict.get("maternal_age")) == None:
+        pass
+    elif maternal_age > 39:
+        risk_score += 9
+
+    # Parity
+    if (parity := json_dict.get("parity")) == None:
+        pass
+    elif 1 <= parity <= 4:
+        risk_score += 1.4
+    elif parity > 4:
+        risk_score += 3.5
+
+    # Multiparity
+    if (multiparity := json_dict.get("multiparity")) == None:
+        pass
+    elif multiparity > 4:
+        risk_score += 2.3
+    elif multiparity > 1:
+        risk_score += 1.9
+
+    # Previous caesarean section
+    if json_dict.get("previous_caesarean_section"):
+        risk_score += 2.7
+    
+    # Abortion
+    if json_dict.get("abortion"):
+        risk_score += 1.9
+
+    # Alcohol during pregnancy
+    if (alcohol := json_dict.get("alcohol")) == None:
+        pass
+    elif alcohol > 0:
+        risk_score += 1.6
+    
+    # Gynacological diseases
+    if json_dict.get("gynacological_diseases"):
+        risk_score += 2.4
+    
+    # Smoking and cocaine use during pregnancy
+    if json_dict.get("smoking") and json_dict("cocaine"):
+        risk_score += 1.6
+
+    # TODO: These last 3 factors need to be implemented
+    # Previous history of evacuation of the uterus or dilation and curettage
+    
+    # Delivery by caesarean section in previous pregnancy 
+
+    # Recurrent vaginal bleeding during the current pregnancy
+
+    return placenta_praevia_risk(risk_score)
---
+++ server/epr/epr/services/complications/placental_abruption.py
@@ -0,0 +1,68 @@
+from typing import get_args
+from ...exceptions.api_exceptions import InternalServerError
+
+
+# Returning the risk_score and the severity of the risk_score
+def placental_abruption_risk(risk_score: int) -> dict:
+    # TODO: This needs to be specified.
+    return
+
+# Calculating risk score unique for post-partum depression
+
+
+def calculate(json_dict: dict) -> dict:
+    risk_score = 0
+
+    # Hypertension
+    if json_dict.get("hypertension"):
+        risk_score += 2
+
+    # Preeclampsia
+    if json_dict.get("preeclampsia"):
+        risk_score += 2
+
+    # Fetal Growth Reduction
+    if json_dict.get("fetal_growth_reduction"):
+        risk_score += 3
+
+    # Age
+    if (age := json_dict.get("age")) == None:
+        pass
+    elif age >= 35:
+        risk_score += 1.5
+
+    # Irregular prenatal care
+    if json_dict.get("irregular_prenatal_care"):
+        risk_score += 3
+
+    # Parity
+    if (parity := json_dict.get("parity")) == None:
+        pass
+    elif 1 <= parity <= 2:
+        risk_score += 2
+    elif parity == 3:
+        risk_score += 3
+
+    # Anemia
+    if (anemia := json_dict.get("anemia")) == None:
+        pass
+    elif anemia == 0: # Mild anemia
+        2
+    elif anemia == 1: # Moderate anemia
+        3
+    elif anemia == 2: # Severe anemia
+        5
+
+    # Premature rupture of the membranes
+    if json_dict.get("premature_rupture_of_membranes"):
+        risk_score += 2
+
+    # Antepartum haemorrhage
+    if json_dict.get("antepartum_haemorrhage"):
+        risk_score += 25
+    
+    # Placenta Paevia
+    if json_dict.get("placenta_paevia"):
+        risk_score += 3
+
+    return placental_abruption_risk(risk_score)
